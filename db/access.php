231127340f4f4ccdc951b606b1cd619cd14c5ef9
JotaDF <46792070+JotaDF@users.noreply.github.com>
2020-07-08 16:38:41 -0300

    Moodle.org plugin validation fixes
    Moodle.org plugin validation fixes

--- db/access.php
+++ db/access.php
@@ -26,27 +26,22 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
- 
     'block/game:myaddinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
- 
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
- 
     'block/game:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
- 
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ),
- 
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
 );
