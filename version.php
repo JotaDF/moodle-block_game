338c8ce3cea95743d60a86a583d4f0000be8bc93
JotaDF <j.wilson.df@gmail.com>
2021-03-29 22:21:00 -0300

    Atualizações e correções
    Atualizações e correções

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021032916;
-$plugin->requires = 2015051100;
+$plugin->version  = 2021032918;
+$plugin->requires = 2017111300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release  = 'Version for Moodle 3.4 onwards';
 $plugin->component = 'block_game';
