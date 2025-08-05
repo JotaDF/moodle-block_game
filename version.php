c1af26fac23a6380acc73f774ad534badac440da
JotaDF <j.wilson.df@gmail.com>
2025-08-05 12:09:19 -0300

    Ajustes nos templates

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025072540;
+$plugin->version  = 2025072542;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release  = '2.0 Version for Moodle 5.0 onwards';
